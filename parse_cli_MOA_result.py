50f96d1a87e289fbea702e679e96cd46e1c46081
Pavel Netolický <pavel.netolicky@gmail.com>
2016-04-21 16:46:09 +0200

    Special Parser for MOA results. A special work for diploma thesis. TODO: Create universal resultParser

---
+++ parse_cli_MOA_result.py
@@ -0,0 +1,137 @@
+import os
+import sys
+from collections import OrderedDict
+
+from src.TextWriter import TextWriter
+
+# CLI PARSING
+
+# RUN: python parse_cli_MOA_result.py <full path to file>
+
+# Read argument - input file
+if len(sys.argv) == 1:
+    exit('You must enter full path to input file.')
+path_to_file = sys.argv[1]
+try:
+    input_file = open(path_to_file)
+except IOError:
+    exit('File ' + path_to_file + ' does not exist or could not be read.')
+
+
+# Prepare variables
+data = OrderedDict()
+filename = os.path.basename(path_to_file).split('.')[0]
+nameAlgo = os.path.basename(path_to_file).split('_')[2]
+nameModel = os.path.basename(path_to_file).split('_')[3]
+
+# Find out file name + algo name
+name_line = input_file.readline().strip()
+data['file_name'] = filename
+
+if nameAlgo == 'HT':
+    algo_name = 'HoeffdingTree'
+elif nameAlgo == 'HTopt':
+    algo_name = 'HoeffdingOptionTree'
+elif nameAlgo == 'HTadapt':
+    algo_name = 'HoeffdingAdaptiveTree'
+elif nameAlgo == 'J48':
+    algo_name = 'J48'
+else:
+    exit('Unsupported algorithm result.')
+# Set algo name
+data['algo_name'] = algo_name
+#  =================  Special version of loading ===============================#
+# if name_line[0:11] == 'Model type:':
+#     data['model_type'] = name_line.split(':')[1].strip()
+#     if data['model_type'] == 'moa.classifiers.trees.HoeffdingTree':
+#         algo_name = 'HoeffdingTree'
+#         data['algo_name'] = algo_name
+#     elif data['model_type'] == 'moa.classifiers.trees.HoeffdingAdaptiveTree':
+#         algo_name = 'HoeffdingAdaptiveTree'
+#         data['algo_name'] = algo_name
+#     elif data['model_type'] == 'moa.classifiers.trees.HoeffdingOptionTree':
+#         algo_name = 'HoeffdingOptionTree'
+#         data['algo_name'] = algo_name
+# elif name_line == 'J48 pruned tree':
+#     # moa.classifiers.meta.WEKAClassifier
+#     algo_name = 'J48'
+#     data['algo_name'] = algo_name
+# else:
+#     exit('Unsupported algorithm result.')
+
+
+if nameModel == "learnModel":
+    while True:
+        line = input_file.readline()
+        if not line:
+            break
+        line_parts = line.split('=')
+
+        # Test tree parts
+        if line_parts and (line_parts[0].strip() == 'model training instances'):
+            data['model training instances'] = line_parts[1].strip().split(' ')[0]
+        elif line_parts and (line_parts[0].strip() == 'model serialized size (bytes)'):
+            data['model serialized size (bytes)'] = line_parts[1].strip().split(' ')[0]
+        elif line_parts and (line_parts[0].strip() == 'tree size (nodes)'):
+            data['tree size (nodes)'] = line_parts[1].strip().split(' ')[0]
+        elif line_parts and (line_parts[0].strip() == 'tree size (leaves)'):
+            data['tree size (leaves)'] = line_parts[1].strip().split(' ')[0]
+        elif line_parts and (line_parts[0].strip() == 'active learning leaves'):
+            data['active learning leaves '] = line_parts[1].strip().split(' ')[0]
+        elif line_parts and (line_parts[0].strip() == 'tree depth'):
+            data['tree depth'] = line_parts[1].strip().split(' ')[0]
+        else:
+            break
+        # Close file
+    input_file.close()
+
+    print data
+
+    # Add data to output file.
+    text_writer = TextWriter('output')
+    header_list = data.keys()
+    data_list = data.values()
+
+    # Result nameFile
+    res_filename = algo_name + nameModel+ '_results'
+
+    # If the results file does not exist, create it and write header to it.
+    if not os.path.isfile('output/' + res_filename + '.csv'):
+        text_writer.write_file(res_filename, [header_list])
+
+    # Write results.
+    text_writer.write_file(res_filename, [data_list])
+
+
+elif nameModel == "EvaluateInterleavedTestThenTrain":
+    header_str = 'learning evaluation instances,evaluation time (cpu seconds),model cost (RAM-Hours),classified instances,classifications correct (percent),Kappa Statistic (percent),Kappa Temporal Statistic (percent),Kappa M Statistic (percent),model training instances,model serialized size (bytes),tree size (nodes),tree size (leaves),active learning leaves,tree depth,active leaf byte size estimate,inactive leaf byte size estimate,byte size estimate overhead'
+    header_list = header_str.split(',')
+
+    while True:
+        line = input_file.readline()
+        if not line:
+            break
+        line_parts = line.strip().split(',')
+        for a in range(0, len(line_parts)):
+            data[header_list[a]] = line_parts[a]
+    # Close file
+    input_file.close()
+
+    print data
+
+    # Add data to output file.
+    text_writer = TextWriter('output')
+    header_list = data.keys()
+    data_list = data.values()
+
+    res_filename = algo_name + nameModel + '_results'
+
+    # If the results file does not exist, create it and write header to it.
+    if not os.path.isfile('output/' + res_filename + '.csv'):
+        text_writer.write_file(res_filename, [header_list])
+
+    # Write results.
+    text_writer.write_file(res_filename, [data_list])
+
+else:
+    exit('Unsupported model result.')  # Get to the results
